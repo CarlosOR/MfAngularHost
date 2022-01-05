94659a64947a5aadf2ca3eeacf4a3eb1a70f6d64
CarlosOR <caor67@gmail.com>
2022-01-05 16:05:40 -0500

    shell configured

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LandingComponent
+  },
+  {
+    path: 'clients',
+    loadChildren: () => import('mfClient/MfClientContentModule')
+    .then(m => m.MfClientContentModule)
+  },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
